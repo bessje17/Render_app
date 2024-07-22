d89f2adee3aae80c3c4141efd79cb1be06701d3d
bessje17 <127861397+bessje17@users.noreply.github.com>
2024-07-22 14:18:56 +0200

    Update Rander_death.py

--- Rander_death.py
+++ Rander_death.py
@@ -137,6 +137,14 @@ def update_graph(start_date, end_date, sexe_values, age_group_values):
                 mode='lines',
                 name=f'Femme ({age_group})'
             ))
+  if len(fig.data) == 1:
+        fig.add_trace(go.Scatter(
+            x=[None],  # Pas de données visibles
+            y=[None],
+            mode='lines',
+            name='',  # Nom pour la légende
+            line=dict(width=0)  # Ligne invisible
+        ))
 
     fig.update_layout(title='Nombre de décès par date', xaxis_title='Date',
                       yaxis_title='Nombre quotidien de décès')
